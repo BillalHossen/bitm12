845ebb3282bf6cf1cc1f4676b27bbd688fe02416
BillalHossen <mbhnubd@gmail.com>
2024-05-12 18:37:48 +0600

    product category project upload

--- Views/Product.php
+++ Views/Product.php
@@ -1,20 +1,11 @@
-<?php //print_r($products);?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Product</title>
-</head>
+<?php include "includes/header.php"; ?>
+
 <body>
-    <div>
+    <div class="container">
         <h1>Product List</h1>
         <div>
             <Label>Select Product</Label>
             <?php foreach ($products as $product) {?>
-
                 <div>
                     <img src="<?php echo $product['image'] ?>" alt="">
                     <div>
@@ -41,7 +32,6 @@
             </thead>
             <?php foreach ($products as $product) { ?>
 
-
             <tr>
                 <td><?php echo $product['id'] ?></td>
                 <td><?php echo $product['name'] ?></td>
@@ -52,5 +42,6 @@
             <?php } ?>
         </table>
     </div>
+    <?php include "includes/footer.php"; ?>
 </body>
 </html>
